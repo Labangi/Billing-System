a60511f69457e51caeb9e35f4de07f690cf7b121
Labangi <labangisreechetamahata@gmail.com>
2026-01-27 12:21:40 +0530

    Update

--- billSystem/models.py
+++ billSystem/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+class user(models.Model):
+    username=models.CharField(max_length=50)
+    email=models.EmailField()
+    role=models.CharField(max_length=50)
+    pwd=models.CharField(max_length=50)
+
+
--- billSystem/views.py
+++ billSystem/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render,redirect
-from 
+from django.http import HttpResponse
+from billSystem.models import *
+import os
+import  requests
+
 
 # Create your views here.
 def index(request):
-    return render('index.html')
+    return render(request,'index.html')
+
+def login(request):
+    return render(request,'login.html')
+
+def loginn(request):
+    pass
