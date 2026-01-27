ae1ffa30de14814e75fc779705483e2ebb99f723
Arijit-Maji-05 <145277006+Arijit-Maji-05@users.noreply.github.com>
2026-01-27 16:23:34 +0530

    Fix login URL to point to the correct view

--- billSystem/urls.py
+++ billSystem/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns=[
     path('',views.index),
-    path('login',views.index),
+    path('login',views.login),
     path('loginn',views.loginn),
     path('logout',views.logout),
 
