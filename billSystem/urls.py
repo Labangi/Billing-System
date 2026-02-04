4a3488611c90bbb3ffd1e39696dc97a6288722ed
Arijit-Maji-05 <145277006+Arijit-Maji-05@users.noreply.github.com>
2026-02-04 14:49:28 +0530

    Add index path to urlpatterns for views

--- billSystem/urls.py
+++ billSystem/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 from . import views
 urlpatterns=[
     path('',views.index),
+    path('index',views.index),
     path('login',views.login),
     path('loginn',views.loginn),
     path('logout',views.logout),
     path('about_us',views.about_us),
     path('contact',views.contact),
-
+    path('services',views.services),
 ]
