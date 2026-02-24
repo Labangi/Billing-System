314f88ce8f0de3f1b283f97f1c2808dddb5b87db
DeepannitaSadhu <sadhudeepannita0@gmail.com>
2026-02-24 16:34:09 +0530

    Refactor URL patterns for consistency and naming
    Updated URL patterns to include trailing slashes and added names for each path.

--- billSystem/urls.py
+++ billSystem/urls.py
@@ -1,18 +1,17 @@
 from django.urls import path
 from . import views
-urlpatterns=[
-    path('',views.index),
-    path('index',views.index),
-    path('login',views.login),
-    path('loginn',views.loginn),
-    path('logout',views.logout),
-    path('about_us',views.about_us),
-    path('contact',views.contact),
-    path('services',views.services),
-    path('admin-dashboard', views.admin_dashboard),
-    path('training-bills', views.training_bills),
-    path('view-users', views.view_users),
-    path('reminder', views.reminder),
 
-
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('index/', views.index, name='index'),
+    path('login/', views.login, name='login'),
+    path('loginn/', views.loginn, name='loginn'),
+    path('logout/', views.logout, name='logout'),
+    path('about-us/', views.about_us, name='about_us'),
+    path('contact/', views.contact, name='contact'),
+    path('services/', views.services, name='services'),
+    path('admin-dashboard/', views.admin_dashboard, name='admin_dashboard'),
+    path('training-bills/', views.training_bills, name='training_bills'),
+    path('view-users/', views.view_users, name='view_users'),
+    path('reminder/', views.reminder, name='reminder'),
 ]
